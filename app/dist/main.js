60b36ebd148afb48a983226c799ef3194d1da3bf
gabrielguiab <gabriel.lisboa@guiabolso.com.br>
2016-12-23 17:47:47 -0200

    Correção do tiro, criação de inimigos e limites

--- app/dist/main.js
+++ app/dist/main.js
@@ -1,12 +1,12 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-// var spaceShip = createImg('images/spaceship.png', 'oies', 'oies')
-// var spaceShip = document.getElementById('spaceShip');
 var spaceShip = new Image;
 spaceShip.src = 'images/spaceship.png';
 
 var myGameArea = {
   keys: [],
+  shooting: false,
+  hasEnemy: false,
 };
 
 spaceShip.onload = function(){
@@ -26,68 +26,104 @@ window.addEventListener('keyup', function (e) {
   myGameArea.keys[e.keyCode] = false;
 })
 
-var shooting = false;
-
 
 function update() {
-  ctx.clearRect(0, 0, c.width, c.height);
   if (myGameArea.keys && myGameArea.keys[37]) {
-    ship.x-= ship.speed;
-    if(!shooting) {
+    if (ship.x - ship.speed > 0) {
+        ship.x-= ship.speed;
+    }
+    if(!myGameArea.shooting) {
       laser.x-= ship.speed;
     }
   }
   if (myGameArea.keys && myGameArea.keys[39]) {
-    ship.x+= ship.speed;
-    if(!shooting) {
+    if (ship.x + ship.speed < (c.width - ship.width)) {
+        ship.x+= ship.speed;
+    }
+    if(!myGameArea.shooting) {
       laser.x+= ship.speed;
     }
   }
   if (myGameArea.keys && myGameArea.keys[38]) {
-    ship.y-= ship.speed;
-    if(!shooting) {
+    if (ship.y - ship.speed > 0) {
+      ship.y-= ship.speed;
+    }
+    if(!myGameArea.shooting) {
       laser.y-= ship.speed;
     }
   }
   if (myGameArea.keys && myGameArea.keys[40]) {
-    ship.y+= ship.speed;
-    if(!shooting) {
+    if (ship.y + ship.speed < (c.height - ship.height)) {
+      ship.y+= ship.speed;
+    }
+    if(!myGameArea.shooting) {
       laser.y+= ship.speed;
     }
   }
   if (myGameArea.keys && myGameArea.keys[32]) {
-    shoot();
+      if(!myGameArea.shooting) {
+          myGameArea.shooting = true;
+      }
   }
+
+  ctx.clearRect(0, 0, c.width, c.height);
   // ship
   ctx.drawImage(ship.img, ship.x, ship.y, ship.width, ship.height);
-  if(shooting) {
-    // laser
-    ctx.rect(laser.x, laser.y, laser.width, laser.height);
-    ctx.strokeStyle="#FF0000";
-    ctx.stroke();
+
+  // laser
+  if(myGameArea.shooting) {
+    shoot();
   }
+  if(!myGameArea.hasEnemy) {
+      generateEnemy(Math.floor((Math.random() * c.width - 20) + 1));
+  } else {
+      moveEnemy();
+  }
+}
+
+function generateEnemy(x) {
+    myGameArea.hasEnemy = true;
+    enemy.x = x;
+    enemy.y = 0;
+    ctx.beginPath();
+    ctx.rect(enemy.x, enemy.y, enemy.width, enemy.height);
+    ctx.strokeStyle="#ff23da";
+    ctx.stroke();
+}
+
+function moveEnemy() {
+    ctx.beginPath();
+    enemy.y += enemy.speed;
+    ctx.rect(enemy.x, enemy.y, enemy.width, enemy.height);
+    ctx.strokeStyle="#ff23da";
+    ctx.stroke();
+    if(enemy.y + enemy.height > c.height) {
+        myGameArea.hasEnemy = false;
+    }
+
+    if((laser.x >= enemy.x && laser.x <= enemy.x + enemy.width) && (laser.y >= enemy.y && laser.y <= enemy.y + enemy.height)) {
+        myGameArea.hasEnemy = false;
+        myGameArea.shooting = false;
+    }
+
 }
 
 function shoot(){
-  console.log(shooting)
-  if(!shooting) {
-    shooting = true;
-    var laserLife = setInterval(function() {
-      laser.y-= laser.speed;
-      if (laser.y + 10 < 0) {
-        laser.y = ship.y;
-        laser.x = ship.x
-        shooting = false;
-        window.clearInterval(laserLife);
-        laserLife = false;
-      }
-    }, 10);
-  }
+    ctx.beginPath();
+    laser.y-= laser.speed;
+    ctx.rect(laser.x, laser.y, laser.width, laser.height);
+    ctx.strokeStyle="#fffb0f";
+    ctx.stroke();
+    if (laser.y - laser.speed < 0) {
+      laser.y = ship.y;
+      laser.x = ship.x
+      myGameArea.shooting = false;
+    }
 }
 
 var ship = {
-  x: 0,
-  y: 0,
+  x: Math.floor((c.width / 2) - 5),
+  y: c.height - 15,
   speed: 1,
   img: spaceShip,
   width: 10,
@@ -99,5 +135,13 @@ var laser = {
   y: 0,
   speed: 5,
   width: 2,
-  height: 2,
+  height: 5,
+}
+
+var enemy = {
+  x: 0,
+  y: 0,
+  speed: .5,
+  width: 10,
+  height: 7,
 }
